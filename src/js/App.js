b4513b5c063042a362fe9f8e2bbb8e6f8ac82718
Peter Hellstrand <tbarbiche@gmail.com>
2018-03-29 20:39:28 +0200

    imports working

---
+++ src/js/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.css';
+import CV from './CV';
+
+const App = () =>
+	<div className={styles.container}>
+		<CV />
+	</div>;
+
+export default App;
